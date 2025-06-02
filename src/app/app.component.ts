f6a8aab39767349e3539f1502ac2c2189112c073
oleh-tft <tiftelalbion@gmail.com>
2025-06-02 23:53:47 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, FirstComponent, SecondComponent, ThirdComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
